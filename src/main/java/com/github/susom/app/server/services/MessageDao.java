98eb31ed417b5ccfcf1cc259ccc3ca2dfd8b56e4
Garrick Olson <garricko@stanford.edu>
2016-09-16 17:45:06 -0700

    Tidy up some very minor things

--- src/main/java/com/github/susom/app/server/services/MessageDao.java
+++ src/main/java/com/github/susom/app/server/services/MessageDao.java
@@ -1,16 +1,17 @@
 /*
  * Copyright 2016 The Board of Trustees of The Leland Stanford Junior University.
- * All Rights Reserved.
  *
- * See the NOTICE and LICENSE files distributed with this work for information
- * regarding copyright ownership and licensing. You may not use this file except
- * in compliance with a written license agreement with Stanford University.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See your
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.susom.app.server.services;
 
